b1fa7416408d6a655a8fad08ef79f5ca37cf925a
Leonardo Deodato <odranoelstc2@gmail.com>
2022-02-14 14:52:46 -0300

    Revert "adding files"
    This reverts commit bbc48ef1a0fc3c96bde14f4bc18d4ad6ea134385.

--- src/services/api.jsx
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: "https://api.github.com"
-/*    client_id: "Iv1.b935c0cbc883c7f9",
-    client_secret: "32f5b03015ed722d80acbca4ce653799adf280cb", */
-})
-
-export default api;
